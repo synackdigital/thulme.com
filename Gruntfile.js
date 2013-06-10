ee39c21d8ac2fe2d3d162138b549a25307e864bd
frebro <frebro@gmail.com>
2013-06-10 12:38:21 +0200

    added grunt ftp deploy task for themes directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,24 @@ module.exports = function(grunt) {
         src: 'wordpress',
         dest: './',
         exclusions: ['wordpress/**/.DS_Store', 'wordpress/**/Thumbs.db', 'wordpess/media']
+      },
+      'themes-dev': {
+        auth: {
+          host: 'ftpcluster.loopia.se',
+          port: 21,
+          authKey: 'dev'
+        },
+        src: 'wordpress/wp-content/themes',
+        dest: 'wp-content/themes',
+        exclusions: ['wordpress/wp-content/themes/**/.DS_Store', 'wordpress/wp-content/themes/**/Thumbs.db']
+      },
+      'themes-prod': {
+        auth: {
+          authKey: 'prod'
+        },
+        src: 'wordpress/wp-content/themes',
+        dest: 'wp-content/themes',
+        exclusions: ['wordpress/wp-content/themes/**/.DS_Store', 'wordpress/wp-content/themes/**/Thumbs.db']
       }
     }
   });
