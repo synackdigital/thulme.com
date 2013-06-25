cb2f6f80840070eafdf1cfac96e35f6d977e08b8
frebro <frebro@gmail.com>
2013-06-25 14:43:52 +0200

    custom footer template

---
+++ wordpress/wp-content/themes/thulme/templates/footer.php
@@ -0,0 +1,7 @@
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
