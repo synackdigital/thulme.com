ca94df054b91268a301a91e99bafd01d48360cab
frebro <frebro@gmail.com>
2013-06-25 13:38:42 +0200

    site description in header

--- wordpress/wp-content/themes/thulme/templates/header.php
+++ wordpress/wp-content/themes/thulme/templates/header.php
@@ -1,10 +1,11 @@
 <header class="banner" role="banner">
   <div class="container-fluid">
     <a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+    <div class="description"><?php bloginfo('description'); ?></div>
     <nav class="nav-main" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-list'));
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-list nav-list-vivid'));
         endif;
       ?>
     </nav>
