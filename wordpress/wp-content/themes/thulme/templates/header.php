439509183f37437dcb0e2fe9cc8b5a8f9ca1e266
frebro <frebro@gmail.com>
2013-06-26 18:37:12 +0200

    nav styling

--- wordpress/wp-content/themes/thulme/templates/header.php
+++ wordpress/wp-content/themes/thulme/templates/header.php
@@ -5,14 +5,14 @@
     <nav class="nav nav-primary" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-list'));
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-list nav-list-vivid'));
         endif;
       ?>
     </nav>
     <nav class="nav nav-secondary" role="navigation">
       <?php
         if (has_nav_menu('secondary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'secondary_navigation', 'menu_class' => 'nav nav-list'));
+          wp_nav_menu(array('theme_location' => 'secondary_navigation', 'menu_class' => 'nav nav-list nav-list-vivid'));
         endif;
       ?>
     </nav>
