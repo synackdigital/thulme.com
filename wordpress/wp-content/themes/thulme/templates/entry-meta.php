475a7ddb52596b2cd55ed868d6093b344e3726f5
frebro <frebro@gmail.com>
2013-06-27 17:17:50 +0200

    layout styling

--- wordpress/wp-content/themes/thulme/templates/entry-meta.php
+++ wordpress/wp-content/themes/thulme/templates/entry-meta.php
@@ -1,1 +1,1 @@
-<span class="label label-primary"><time class="updated" datetime="<?php echo get_the_time('c'); ?>" pubdate><?php echo get_the_date(); ?></time></span>
+<span class="label"><time class="updated" datetime="<?php echo get_the_time('c'); ?>" pubdate><?php echo get_the_date(); ?></time></span>
