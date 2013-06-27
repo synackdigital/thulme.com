482be8bd59c3545e5c7edc1e3a822f775d0c7ebe
frebro <frebro@gmail.com>
2013-06-27 14:08:37 +0200

    added post thumbnails

---
+++ wordpress/wp-content/themes/thulme/templates/content-single.php
@@ -0,0 +1,18 @@
+<?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <?php
+        if (has_post_thumbnail() ) // check if the post has a Post Thumbnail assigned to it.
+          the_post_thumbnail(); ?>
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <?php get_template_part('templates/entry-meta'); ?>
+    </header>
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
+    <footer>
+      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+    </footer>
+    <?php comments_template('/templates/comments.php'); ?>
+  </article>
+<?php endwhile; ?>
