3d21e1b9829606e229f484b16dfe4afdd491e30a
frebro <frebro@gmail.com>
2013-07-08 00:13:49 +0200

    removed the footer

--- wordpress/wp-content/themes/thulme/templates/footer.php
+++ wordpress/wp-content/themes/thulme/templates/footer.php
@@ -1,8 +1,3 @@
-<footer class="content-info" role="contentinfo">
-  <div class="colophon">
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-  </div>
-</footer>
 
 <!-- Fonts from Hoefler & Frere-Jones -->
 <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7770232/774582/css/fonts.css">
