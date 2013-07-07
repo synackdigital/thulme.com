2344119aa46b2d3db8517f964a0bdd8862ce3799
frebro <frebro@gmail.com>
2013-07-08 00:08:36 +0200

    implemented h&fj cloud fonts

--- wordpress/wp-content/themes/thulme/templates/footer.php
+++ wordpress/wp-content/themes/thulme/templates/footer.php
@@ -4,4 +4,7 @@
   </div>
 </footer>
 
+<!-- Fonts from Hoefler & Frere-Jones -->
+<link rel="stylesheet" type="text/css" href="//cloud.typography.com/7770232/774582/css/fonts.css">
+
 <?php wp_footer(); ?>
