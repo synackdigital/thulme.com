4d206d9493a934d64dc16105fa59043297aad269
frebro <frebro@gmail.com>
2013-07-08 10:57:03 +0200

    font import in header fixes fut

--- wordpress/wp-content/themes/thulme/functions.php
+++ wordpress/wp-content/themes/thulme/functions.php
@@ -21,6 +21,9 @@ function thulme_scripts() {
   wp_enqueue_script('jquery-placeholder', get_stylesheet_directory_uri() . '/assets/js/vendor/jquery.placeholder.js', array('jquery'), null, true);
   wp_enqueue_script('jquery-stacktable', get_stylesheet_directory_uri() . '/assets/js/vendor/jquery.stacktable.js', array('jquery'), null, true);
 
+  // Enqueue font styles from Typography.com
+  wp_enqueue_style('typography_com', '//cloud.typography.com/7770232/774582/css/fonts.css', false, null);
+
   // Enqueue theme scripts
   wp_enqueue_script('thulme_plugins', get_stylesheet_directory_uri() . '/assets/js/plugins.js', false, null, true);
   wp_enqueue_script('thulme_main', get_stylesheet_directory_uri() . '/assets/js/main.js', false, null, true);
--- wordpress/wp-content/themes/thulme/templates/footer.php
+++ wordpress/wp-content/themes/thulme/templates/footer.php
@@ -1,5 +1,2 @@
 
-<!-- Fonts from Hoefler & Frere-Jones -->
-<link rel="stylesheet" type="text/css" href="//cloud.typography.com/7770232/774582/css/fonts.css">
-
 <?php wp_footer(); ?>
