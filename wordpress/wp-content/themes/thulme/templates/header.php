49be2a2f98252f3b7a78ab8ed7f341f6b81b5246
frebro <frebro@gmail.com>
2013-07-12 16:18:17 +0200

    implemented menu items for post formats

--- wordpress/wp-content/themes/thulme/templates/header.php
+++ wordpress/wp-content/themes/thulme/templates/header.php
@@ -13,6 +13,11 @@
     </nav>
     <?php get_search_form(); ?>
     <nav class="nav nav-secondary" role="navigation">
+      <ul id="menu-types" class="nav nav-list">
+        <li class="menu-type-link"><a href="<?php echo get_post_format_link( 'link' ); ?>"><span class="icon-calendar-empty"></span> Decks</a></li>
+        <li class="menu-type-video"><a href="<?php echo get_post_format_link( 'video' ); ?>"><span class="icon-youtube-play"></span> Videos</a></li>
+        <li class="menu-type-video"><a href="<?php echo get_post_format_link( 'audio' ); ?>"><span class="icon-volume-up"></span> Radio</a></li>
+      </ul>
       <?php
         if (has_nav_menu('secondary_navigation')) :
           wp_nav_menu(array('theme_location' => 'secondary_navigation', 'menu_class' => 'nav nav-list'));
