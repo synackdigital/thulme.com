deeb2221eed804f5ee970276afe8f92e97993b9f
frebro <frebro@gmail.com>
2013-07-14 17:42:01 +0200

    forgot to add files in previous commits

---
+++ wordpress/wp-content/themes/thulme/search.php
@@ -0,0 +1,37 @@
+<?php get_template_part('templates/page', 'header'); ?>
+
+<?php if (!have_posts()) : ?>
+  <div class="alert">
+    <?php _e('Sorry, no results were found.', 'roots'); ?>
+  </div>
+  <?php get_search_form(); ?>
+<?php endif; ?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <?php if ( has_post_format( 'link' )) : ?>
+        <span class="entry-icon icon-calendar-empty" aria-hidden="true"></span>
+      <?php elseif ( has_post_format( 'audio' )) : ?>
+        <span class="entry-icon icon-volume-up" aria-hidden="true"></span>
+      <?php elseif ( has_post_format( 'video' )) : ?>
+        <span class="entry-icon icon-youtube-play" aria-hidden="true"></span>
+      <?php else: ?>
+        <span class="entry-icon icon-pencil" aria-hidden="true"></span>
+      <?php endif; ?>
+      <a href="<?php the_permalink(); ?>">
+        <h2 class="entry-title"><?php the_title(); ?></h2>
+        <?php get_template_part('templates/entry-meta'); ?>
+      </a>
+    </header>
+  </article>
+<?php endwhile; ?>
+
+<?php if ($wp_query->max_num_pages > 1) : ?>
+  <nav class="post-nav">
+    <ul class="pager">
+      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+    </ul>
+  </nav>
+<?php endif; ?>
