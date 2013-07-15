6d5f2ce9c908595938bfa13785a66e32b3cd57e0
frebro <frebro@gmail.com>
2013-07-15 17:41:36 +0200

    script uglification

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,35 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+  var js_src = 'wordpress/wp-content/themes/thulme/assets/js';
+
   // Project configuration.
   grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Uglify JS
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        preserveComents: 'some'
+      },
+      build: {
+        src: [
+          js_src+'/vendor/jquery-ui-1.10.3.custom.min.js',
+          js_src+'/vendor/jquery.ui.touch-punch.min.js',
+          js_src+'/vendor/bootstrap-select.js',
+          js_src+'/vendor/bootstrap-switch.js',
+          js_src+'/vendor/flatui-checkbox.js',
+          js_src+'/vendor/flatui-radio.js',
+          js_src+'/vendor/jquery.tagsinput.js',
+          js_src+'/vendor/jquery.placeholder.js',
+          js_src+'/vendor/jquery.stacktable.js',
+          js_src+'/vendor/looper.js'
+        ],
+        dest: js_src+'/plugins.js'
+      }
+    },
 
     // LESS compile/compress
     less: {
@@ -33,6 +60,7 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
--- wordpress/wp-content/themes/thulme/functions.php
+++ wordpress/wp-content/themes/thulme/functions.php
@@ -10,29 +10,12 @@ function thulme_scripts() {
   wp_dequeue_style('roots_bootstrap_responsive');
   wp_deregister_style('roots_bootstrap_responsive');
 
-  // Deregister Roots main styles
-  wp_deregister_style('roots_main');
-
-  // Enqueue scripts for Flat-UI
-  wp_enqueue_script('jquery-ui', get_stylesheet_directory_uri() . '/assets/js/vendor/jquery-ui-1.10.3.custom.min.js', array('jquery'), null, true);
-  wp_enqueue_script('jquery-ui-touch-punch', get_stylesheet_directory_uri() . '/assets/js/vendor/jquery.ui.touch-punch.min.js', array('jquery','jquery-ui'), null, true);
-  wp_enqueue_script('bootstrap-select', get_stylesheet_directory_uri() . '/assets/js/vendor/bootstrap-select.js', array('jquery','roots_bootstrap'), null, true);
-  wp_enqueue_script('bootstrap-switch', get_stylesheet_directory_uri() . '/assets/js/vendor/bootstrap-switch.js', array('jquery','roots_bootstrap'), null, true);
-  wp_enqueue_script('flatui-checkbox', get_stylesheet_directory_uri() . '/assets/js/vendor/flatui-checkbox.js', array('jquery','roots_bootstrap'), null, true);
-  wp_enqueue_script('flatui-radio', get_stylesheet_directory_uri() . '/assets/js/vendor/flatui-radio.js', array('jquery','roots_bootstrap'), null, true);
-  wp_enqueue_script('jquery-tagsinput', get_stylesheet_directory_uri() . '/assets/js/vendor/jquery.tagsinput.js', array('jquery'), null, true);
-  wp_enqueue_script('jquery-placeholder', get_stylesheet_directory_uri() . '/assets/js/vendor/jquery.placeholder.js', array('jquery'), null, true);
-  wp_enqueue_script('jquery-stacktable', get_stylesheet_directory_uri() . '/assets/js/vendor/jquery.stacktable.js', array('jquery'), null, true);
-
-  // Enqueue Looper
-  wp_enqueue_script('jquery-looper', get_stylesheet_directory_uri() . '/assets/js/vendor/looper.js', array('jquery'), null, true);
-
   // Enqueue font styles from Typography.com
   wp_enqueue_style('typography_com', '//cloud.typography.com/7770232/774582/css/fonts.css', false, null);
 
   // Enqueue theme scripts
-  wp_enqueue_script('thulme_plugins', get_stylesheet_directory_uri() . '/assets/js/plugins.js', false, null, true);
-  wp_enqueue_script('thulme_main', get_stylesheet_directory_uri() . '/assets/js/main.js', false, null, true);
+  wp_enqueue_script('thulme_plugins', get_stylesheet_directory_uri() . '/assets/js/plugins.js', array('jquery'), null, true);
+  wp_enqueue_script('thulme_main', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery', 'thulme_plugins'), null, true);
 }
 add_action('wp_enqueue_scripts', 'thulme_scripts', 200);
 
