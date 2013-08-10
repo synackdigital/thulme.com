b8ab0a58831f3f14147b67bae823d97e7aea4656
frebro <frebro@gmail.com>
2013-08-10 11:27:17 +0200

    removed label style from post dates

--- wordpress/wp-content/themes/thulme/templates/entry-meta.php
+++ wordpress/wp-content/themes/thulme/templates/entry-meta.php
@@ -1,1 +1,1 @@
-<span class="label"><time class="updated" datetime="<?php echo get_the_time('c'); ?>" pubdate><?php echo get_the_date(); ?></time></span>
+<small><time class="updated" datetime="<?php echo get_the_time('c'); ?>" pubdate><?php echo get_the_date(); ?></time></small>
