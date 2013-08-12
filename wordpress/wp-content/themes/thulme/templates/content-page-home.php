aa9e1e6dba1a76c5c27dddcd92a997c5dd09b9bd
frebro <frebro@gmail.com>
2013-08-12 09:14:17 +0200

    remover document class from home page listings

--- wordpress/wp-content/themes/thulme/templates/content-page-home.php
+++ wordpress/wp-content/themes/thulme/templates/content-page-home.php
@@ -11,7 +11,7 @@
   <section class="category category-open-design">
     <h2 class="category-name"><a href="<?php echo get_category_link( $category_open_design->term_id ); ?>" title="Show all posts in <?php echo $category_open_design->name; ?>"><?php echo $category_open_design->name; ?></a></h2>
     <p class="category-description"><small><?php echo $category_open_design->description; ?></small></p>
-    <ul class="unstyled document">
+    <ul class="unstyled">
       <?php while (have_posts()) : the_post(); ?>
       <li class="hentry">
         <?php if ( has_post_format( 'link' )) : ?>
@@ -37,7 +37,7 @@
   <section class="category category-startup-tools">
     <h2 class="category-name"><a href="<?php echo get_category_link( $category_startup_tools->term_id ); ?>" title="Show all posts in <?php echo $category_startup_tools->name; ?>"><?php echo $category_startup_tools->name; ?></a></h2>
     <p class="category-description"><small><?php echo $category_startup_tools->description; ?></small></p>
-    <ul class="unstyled document">
+    <ul class="unstyled">
       <?php while (have_posts()) : the_post(); ?>
       <li class="hentry">
         <?php if ( has_post_format( 'link' )) : ?>
@@ -61,7 +61,7 @@
   <section class="category category-weiji">
     <h2 class="category-name"><a href="<?php echo get_category_link( $category_weiji->term_id ); ?>" title="Show all posts in <?php echo $category_weiji->name; ?>"><?php echo $category_weiji->name; ?></a></h2>
     <p class="category-description"><small><?php echo $category_weiji->description; ?></small></p>
-    <ul class="unstyled document">
+    <ul class="unstyled">
       <?php while (have_posts()) : the_post(); ?>
       <li class="hentry">
         <?php if ( has_post_format( 'link' )) : ?>
