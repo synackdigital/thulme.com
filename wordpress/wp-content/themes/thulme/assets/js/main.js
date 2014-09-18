40efdddae0180dd58e2940245f5b3ac2fbe3a78f
frebro <frebro@gmail.com>
2014-09-18 11:42:28 +0200

    clickable headlines on home page, close #8

--- wordpress/wp-content/themes/thulme/assets/js/main.js
+++ wordpress/wp-content/themes/thulme/assets/js/main.js
@@ -1,10 +1,24 @@
 $(function(){
-  console.log('Hello world');
 
-  // Navigation toggle
-  $('#nav-toggle').click(function(){
-    $('.navigation', '.banner').slideToggle('quick');
-  });
+  // Mobile nav behaviour on home page
+  if ( $('#nav-toggle').css('display') != 'none' ) {
+
+    // Nav toggle button
+    $('#nav-toggle').click(function(){
+      $('.navigation', '.banner').slideToggle('quick');
+    });
+
+    // Section nav
+    $('section.category', 'body.home').each( function( index ) {
+      $entries = $('ul', $(this));
+      $entries.hide();
+
+      $('.category-name a', $(this)).click( $.proxy( function( event ) {
+        event.preventDefault();
+        $('ul', $(this)).slideToggle('quick');
+      }, this ));
+    })
+  }
 
   // Loop Twitter feed
   $('.widget_twitter ul').addClass('looper-inner').wrap('<div class="looper slide up" />');
@@ -12,3 +26,4 @@ $(function(){
   $('.widget_twitter .looper').looper('to', 1);
   $('.content-info .widget_twitter').show();
 });
+
